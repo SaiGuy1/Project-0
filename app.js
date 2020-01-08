448f385b4402b17988485d41ea36e7e24f6b9444
Sai Narendran <Snarendran0304@gmail.com>
2020-01-08 12:07:31 -0800

    Created Initial Commit
    Realized most of my code was directly from Eloquent Javascript's Platform Game Tutorial so rebuilding project from scratch so I can add all functionality myself

---
+++ app.js
@@ -0,0 +1,20 @@
+var simpleLevelPlan = `
+......................
+..#................#..
+..#..............=.#..
+..#.........o.o....#..
+..#.@......#####...#..
+..#####............#..
+......#++++++++++++#..
+......##############..
+......................`;
+
+var Level = class Level {
+  constructor(plan) {
+    let rows = plan.trim().split("\n").map(l => [...l]);
+    this.height = rows.length;
+    this.width = rows[0].length;
+    this.startActors = [];
+    });
+  }
+}
